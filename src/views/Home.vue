265bf5ee9e6ca6a4d40b0fdef71434a9b78ba99e
tuhuaqing <2438974094@qq.com>
2020-02-12 22:36:07 +0800

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,12 @@
     <div class="center">
       <h1 class="green-color">首页</h1>
       <img class="vue-logo" alt="Vue logo" src="../assets/logo.png" />
+      <p class="text">该版本目前仅有 登录,注册,修改头像,昵称等功能</p>
+      <p class="text">敬请期待后续更新</p>
+      <p class="text">
+        可以加作者QQ:
+        <span style="color:dodgerblue;" id="copy" @click="copy">2438974094</span>
+      </p>
     </div>
   </div>
 </template>
@@ -12,14 +18,20 @@
 export default {
   name: 'home',
   components: {
+  },
+  methods: {
+    copy() {
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .vue-logo{
-    width: 5rem;
-    height: 5rem;
-  }
-
+.vue-logo {
+  width: 5rem;
+  height: 5rem;
+}
+.text {
+  font-size: 0.4rem;
+}
 </style>
